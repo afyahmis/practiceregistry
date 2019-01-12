f8f6a6b521237a330093cb3b3447eaebaadd8161
Koske Kimutai <koskedk@live.com>
2019-01-12 17:27:25 +0300

    Refactore tests to clear db

--- test/infrastructure/PracticeTypeRepository.test.ts
+++ test/infrastructure/PracticeTypeRepository.test.ts
@@ -1,5 +1,5 @@
 import {Container} from "typedi";
-import {createTestPracticeTypes, initDbConnection} from "../test-initializer";
+import {clearDb, createTestPracticeTypes, initDbConnection} from "../test-initializer";
 import {PracticeTypeRepository} from '../../src/infrastructure/PracticeTypeRepository';
 import {PracticeType} from '../../src/core/model/domain/PracticeType';
 
@@ -9,34 +9,36 @@ describe("PracticeTypeRepository Tests", () => {
     let practiceTypes = createTestPracticeTypes();
 
     beforeAll(async () => {
+        clearDb();
         await initDbConnection();
         repository = Container.get(PracticeTypeRepository);
     });
 
-    it("should create type", async () => {
+    it("should create practiceType", async () => {
         const practiceType = await repository.createOrUpdate(new PracticeType("Facility", "Facility"));
         expect(practiceType).not.toBeUndefined();
         console.log(`${practiceType}`);
     });
-    it("should create batch entities", async () => {
+
+    it("should create batch practiceTypes", async () => {
         const pTypes = await repository.createOrUpdateBatch(practiceTypes);
         expect(pTypes.length === 2);
         pTypes.forEach((p) => console.log(`${p}`));
     });
 
-    test("should read  entities", async () => {
+    test("should read practiceTypes", async () => {
         const pTypes = await repository.getAll();
         expect(pTypes.length > 0);
         pTypes.forEach((p) => console.log(`${p}`));
     });
 
-    it("should find  entity by id", async () => {
+    it("should find practiceType by id", async () => {
         const practiceType = await repository.get(practiceTypes[0].id);
         expect(practiceType).not.toBeUndefined();
         console.log(`${practiceType}`);
     });
 
-    it("should delete  entity by id", async () => {
+    it("should delete practiceType by id", async () => {
         const carId = practiceTypes[1].id;
         await repository.remove(carId);
         const testCar = await repository.get(carId);
--- test/test-initializer.ts
+++ test/test-initializer.ts
@@ -1,14 +1,27 @@
+import * as fs from "fs";
 import {createConnection, useContainer} from "typeorm";
 import {Container} from "typedi";
 import {PracticeType} from "../src/core/model/domain/PracticeType";
 import * as faker from "faker";
 
+const dbPath: string = "test/practiceTest.sqlite";
+
 useContainer(Container);
+
+export let clearDb= ()=> {
+    fs.unlink(dbPath, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        }
+    );
+}
+
 export let initDbConnection = async () => {
     await createConnection({
             logging: false,
             type: "sqlite",
-            database: ":memory",
+            database: dbPath,
             entities: ["./src/core/model/domain/*.ts"],
             synchronize: true
         }
