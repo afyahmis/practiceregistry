9a98b8a7194d99d5cb709a41dc23fadc15df6147
Koske Kimutai <koskedk@live.com>
2019-01-12 17:09:32 +0300

    PracticeTypeRepository tests

--- test/infrastructure/PracticeTypeRepository.test.ts
+++ test/infrastructure/PracticeTypeRepository.test.ts
@@ -1,11 +1,12 @@
-import { Container } from "typedi";
-import { initDbConnection } from "../test-initializer";
-import { PracticeTypeRepository } from '../../src/infrastructure/PracticeTypeRepository';
-import { PracticeType } from '../../src/core/model/domain/PracticeType';
+import {Container} from "typedi";
+import {createTestPracticeTypes, initDbConnection} from "../test-initializer";
+import {PracticeTypeRepository} from '../../src/infrastructure/PracticeTypeRepository';
+import {PracticeType} from '../../src/core/model/domain/PracticeType';
 
 describe("PracticeTypeRepository Tests", () => {
 
     let repository: PracticeTypeRepository;
+    let practiceTypes = createTestPracticeTypes();
 
     beforeAll(async () => {
         await initDbConnection();
@@ -13,8 +14,33 @@ describe("PracticeTypeRepository Tests", () => {
     });
 
     it("should create type", async () => {
-        const testCar = await repository.createOrUpdate(new PracticeType("Facility","Facility"));
-        expect(testCar).not.toBeUndefined();
-        console.log(`${testCar}`);
+        const practiceType = await repository.createOrUpdate(new PracticeType("Facility", "Facility"));
+        expect(practiceType).not.toBeUndefined();
+        console.log(`${practiceType}`);
     });
+    it("should create batch entities", async () => {
+        const pTypes = await repository.createOrUpdateBatch(practiceTypes);
+        expect(pTypes.length === 2);
+        pTypes.forEach((p) => console.log(`${p}`));
+    });
+
+    test("should read  entities", async () => {
+        const pTypes = await repository.getAll();
+        expect(pTypes.length > 0);
+        pTypes.forEach((p) => console.log(`${p}`));
+    });
+
+    it("should find  entity by id", async () => {
+        const practiceType = await repository.get(practiceTypes[0].id);
+        expect(practiceType).not.toBeUndefined();
+        console.log(`${practiceType}`);
+    });
+
+    it("should delete  entity by id", async () => {
+        const carId = practiceTypes[1].id;
+        await repository.remove(carId);
+        const testCar = await repository.get(carId);
+        expect(testCar).toBeUndefined();
+    });
+
 });
--- test/test-initializer.ts
+++ test/test-initializer.ts
@@ -1,7 +1,25 @@
-import { createConnection, useContainer } from "typeorm";
-import { Container } from "typedi";
+import {createConnection, useContainer} from "typeorm";
+import {Container} from "typedi";
+import {PracticeType} from "../src/core/model/domain/PracticeType";
+import * as faker from "faker";
 
 useContainer(Container);
 export let initDbConnection = async () => {
-    await createConnection("test");
+    await createConnection({
+            logging: false,
+            type: "sqlite",
+            database: ":memory",
+            entities: ["./src/core/model/domain/*.ts"],
+            synchronize: true
+        }
+    );
 };
+
+export let createTestPracticeTypes = (count: number = 2): Array<PracticeType>=> {
+    let i: number;
+    const practiceTypes: Array<PracticeType> = [];
+    for (i = 0; i < count; i++) {
+        practiceTypes.push(new PracticeType(faker.commerce.productMaterial(), faker.commerce.productMaterial()));
+    }
+    return practiceTypes;
+}
